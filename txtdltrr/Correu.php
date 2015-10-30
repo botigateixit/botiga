ac217d800d7b27a1f4198e97875db69d53055f85
Jordi Sastre <sastrej@gmail.com>
2015-10-31 00:42:59 +0100

    Set de la zona horaria en el correu.php

--- txtdltrr/Correu.php
+++ txtdltrr/Correu.php
@@ -5,7 +5,7 @@
 
 //SMTP needs accurate times, and the PHP time zone MUST be set
 //This should be done in your php.ini, but this is how to do it if you don't have access to that
-date_default_timezone_set('Etc/UTC');
+date_default_timezone_set('Europe/Madrid');
 
 require './PHPMailer/PHPMailerAutoload.php';
 
