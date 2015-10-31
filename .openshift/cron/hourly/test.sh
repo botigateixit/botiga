5143fa719091914c68b565110588df9abd722485
Jordi Sastre <sastrej@gmail.com>
2015-10-31 11:04:29 +0100

    Adaptacio script de cron per posicionar-se en el directori adequat

--- .openshift/cron/hourly/test.sh
+++ .openshift/cron/hourly/test.sh
@@ -5,8 +5,9 @@ export TZ="/usr/share/zoneinfo/Europe/Madrid"
 hora=$(date +%H)
 dia=$(date +%u)
 
-if [ "$hora" -eq "01" ]; then
-   php ${OPENSHIFT_REPO_DIR}/txtdltrr/Correu.php
+if [ "$hora" -eq "12" ]; then
+   cd ${OPENSHIFT_REPO_DIR}/txtdltrr
+   php Correu.php
    date >> ${OPENSHIFT_PHP_LOG_DIR}/test.log
    echo "Enviament de correu"  >> ${OPENSHIFT_PHP_LOG_DIR}/test.log
 	if [ "$dia" -eq "01" ]; then
